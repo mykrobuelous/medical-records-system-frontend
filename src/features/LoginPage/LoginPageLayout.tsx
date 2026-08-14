714e2a98cfbf635bdce4dbcc31adab5566bfa8b2
Myko Bacal <mykobacal23@gmail.com>
2026-08-14 20:17:38 +0800

    INIT:Console Check

--- src/features/LoginPage/LoginPageLayout.tsx
+++ src/features/LoginPage/LoginPageLayout.tsx
@@ -65,6 +65,8 @@ const LoginPageLayout: React.FC<Props> = ({ className }) => {
         }
     };
 
+    console.log('Vite URL', import.meta.env.VITE_API_URL);
+
     return (
         <div
             className={twMerge(
